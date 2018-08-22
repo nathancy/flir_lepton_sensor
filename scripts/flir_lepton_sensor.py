f4cb79e72be5a3c6c0f8963e69ff1ec65a584e4e
Nathan Lam <nathancy@hawaii.edu>
2018-08-22 11:51:57 -1000

    LED status indicator for active/off script

--- scripts/flir_lepton_sensor.py
+++ scripts/flir_lepton_sensor.py
@@ -5,6 +5,7 @@ from pylepton.Lepton3 import Lepton3
 import datetime
 import time
 import os
+import RPi.GPIO as GPIO
 
 class flirLepton3Sensor(object):
     def __init__(self, device = "/dev/spidev0.0"):
@@ -57,3 +58,43 @@ class flirLepton3Sensor(object):
     def create_directory(self, path):
         if not os.path.exists(path):
             os.makedirs(path)
+
+    def status_LED_enable(self):
+
+        # The green ACT LED (led0) has triggers which let some other part of the kernel control the LED. The default trigger for the LED is 'mmc0' which actives when the SD card is accessed. Therese two lines deactivate the mmc0 trigger. GPIO16 can be used to control the LED, its active-low so to turn the LED on, set the pin low, and to turn the LED off, set the pin high. 
+
+        # It's important to note that RPi.GPIO will set the GPIO port to be an input when it's finished (it sets to an input any port which the script set to an output, and you have to set it to an output before you can do anything with it, even if it already was). As GPIO16 stops being an output, control via the kernel interface will no longer work.
+        #os.system("echo gpio | sudo tee /sys/class/leds/led0/trigger > /dev/null")
+        #os.system("echo cpu0 | sudo tee /sys/class/leds/led0/trigger > /dev/null")
+
+        # Needs to be BCM. GPIO.BOARD lets you address GPIO ports by periperal
+        # connector pin number, and the LED GPIO isn't on the connector
+        #GPIO.setmode(GPIO.BCM)
+
+        # Set up GPIO output channel
+        #GPIO.setup(16, GPIO.OUT)
+       
+        # On
+        #GPIO.output(16, GPIO.LOW)   
+        #GPIO.cleanup()
+        
+        os.system("echo gpio | sudo tee /sys/class/leds/led0/trigger > /dev/null")
+        os.system("echo 1 | sudo tee /sys/class/leds/led0/brightness > /dev/null")
+
+    def status_LED_disable(self):
+        #os.system("echo gpio | sudo tee /sys/class/leds/led0/trigger > /dev/null")
+        #os.system("echo cpu0 | sudo tee /sys/class/leds/led0/trigger > /dev/null")
+
+        # Needs to be BCM. GPIO.BOARD lets you address GPIO ports by periperal
+        # connector pin number, and the LED GPIO isn't on the connector
+        #GPIO.setmode(GPIO.BCM)
+
+        # Set up GPIO output channel
+        #GPIO.setup(16, GPIO.OUT)
+       
+        # Off
+        #GPIO.output(16, GPIO.HIGH)   
+
+        #GPIO.cleanup()
+        os.system("echo gpio | sudo tee /sys/class/leds/led0/trigger > /dev/null")
+        os.system("echo 0 | sudo tee /sys/class/leds/led0/brightness > /dev/null")
--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,9 @@ from flir_lepton_sensor import flirLepton3Sensor
 
 sensor = flirLepton3Sensor()
 try:
+    sensor.status_LED_enable()
     sensor.thermal_capture_record_constant()
 except KeyboardInterrupt:
     print("Stopping sensor readings")
+    sensor.status_LED_disable()
 
