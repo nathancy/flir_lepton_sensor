4337d0f7c5df336a4d7727671a2e30b331efd12a
Nathan Lam <nathancy@hawaii.edu>
2018-09-04 15:09:27 -0700

    Added resolution density for generated GPS points

--- scripts/KML_generator.py
+++ scripts/KML_generator.py
@@ -1,6 +1,7 @@
 from pykml.factory import KML_ElementMaker as KML
 from lxml import etree
 import csv
+import sys 
 
 kml_file_name = "image_map.kml"
 csv_file_name = "data.csv"
@@ -19,6 +20,15 @@ def Latitude_DMS_to_DD(raw_latitude, direction):
     converted_latitude = latitude_degree + (latitude_minute/60)
     return converted_latitude if direction == "N" else -converted_latitude
 
+# Determines the amount of points to generate, default every point
+# The higher the resolution, the less points are sampled
+def resolution():
+    try:
+        resolution = sys.argv[1]
+    except IndexError:
+        resolution = 1
+    return int(resolution)
+
 doc = KML.Folder()
 
 with open(csv_file_name) as f:
@@ -26,31 +36,33 @@ with open(csv_file_name) as f:
 
     # Remove column description row
     description = next(reader)
+    resolution = resolution()
 
     for index, row in enumerate(reader):
-        try:
-            DMS_latitude = row[1]
-            latitude_direction = row[2]
-            DMS_longitude = row[3]
-            longitude_direction = row[4]
-            image_id = row[11]
+        if index % resolution == 0:
+            try:
+                DMS_latitude = row[1]
+                latitude_direction = row[2]
+                DMS_longitude = row[3]
+                longitude_direction = row[4]
+                image_id = row[11]
 
-            # Convert DMS latitude/longtitude coordinates to DD format
-            DD_latitude = Latitude_DMS_to_DD(float(DMS_latitude), latitude_direction)
-            DD_longitude = Longitude_DMS_to_DD(float(DMS_longitude), longitude_direction)
+                # Convert DMS latitude/longtitude coordinates to DD format
+                DD_latitude = Latitude_DMS_to_DD(float(DMS_latitude), latitude_direction)
+                DD_longitude = Longitude_DMS_to_DD(float(DMS_longitude), longitude_direction)
 
-            # Create KML point and append to list of points
-            point = KML.Placemark(
-                        KML.name(image_id),
-                        KML.Point(
-                            KML.coordinates(str(DD_longitude) + "," +  str(DD_latitude)),
+                # Create KML point and append to list of points
+                point = KML.Placemark(
+                            KML.name(image_id),
+                            KML.Point(
+                                KML.coordinates(str(DD_longitude) + "," +  str(DD_latitude)),
+                                )
                             )
-                        )
-            doc.append(point)
+                doc.append(point)
 
-        except IndexError:
-            # Need to +2 cause CSV doesnt start and index 0 and removed description column row
-            print("Line # " +  str(index + 2) + " is not full")
+            except IndexError:
+                # Need to +2 cause CSV doesnt start and index 0 and removed description column row
+                print("ERROR: Line # " +  str(index + 2) + " is not valid, aborting point")
 
 # Turn coordinate points into .kml file
 outfile = file(kml_file_name, 'w')
@@ -58,4 +70,5 @@ outfile.write('<?xml version="1.0" encoding="UTF-8"?>\n')
 outfile.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
 outfile.write(etree.tostring(doc, pretty_print = True))
 outfile.write('</kml>')
+print("KML file successfully generated!")
 
