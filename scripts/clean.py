835b3fe31b67d9ef84ad81bbfe45062023033431
Nathan Lam <nathancy@hawaii.edu>
2018-09-11 12:37:09 -1000

    Clean temp files, logger tests, photos script

---
+++ scripts/clean.py
@@ -0,0 +1,6 @@
+import os 
+os.system("sudo rm -rf /home/pi/flir_lepton_sensor/scripts/path.txt")
+os.system("sudo rm -rf /home/pi/flir_lepton_sensor/scripts/image.txt")
+os.system("sudo rm -rf /home/pi/flir_lepton_sensor/scripts/*.pyc")
+os.system("sudo rm -rf /home/pi/flir_lepton_sensor/scripts/photos")
+os.system("sudo rm -rf /home/pi/flir_lepton_sensor/scripts/output.log")
